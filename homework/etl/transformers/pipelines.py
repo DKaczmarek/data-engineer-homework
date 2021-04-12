b1fa794175584376ed9f88b0aa6461f3c846e47b
Dominik Kaczmarek <dominik.kaczmarek@deep.bi>
2021-04-12 20:06:59 +0200

    change withColumn methods to select

--- homework/etl/transformers/pipelines.py
+++ homework/etl/transformers/pipelines.py
@@ -8,6 +8,9 @@ from homework.etl.transformers import functions as func
 from homework.common.logger import ModuleLogger
 
 
+import time
+
+
 class DataTransformer(ABC):
     @abstractmethod
     def transform(self, data: DataFrame) -> DataFrame:
@@ -24,15 +27,21 @@ class ArticleWikisVisitTransformer(DataTransformer):
         return data
 
     def parse_raw_events(self, input_data: DataFrame) -> DataFrame:
-        
         return (
-            input_data.withColumn("tokens", parsers.tokenize_raw_events())
-            .withColumn("parameters", parsers.parse_parameters_from_tokens())
-            .withColumn("user_id", parsers.parse_user_id_from_tokens())
-            .withColumn("article_id", parsers.parse_param_value(key="a"))
-            .withColumn("wiki_id", parsers.parse_param_value(key="c"))
-            .withColumn("timestamp", parsers.parse_timestamp_from_tokens())
-            .drop("raw", "tokens", "parameters")
+            input_data.withColumn(
+                "tokens", parsers.tokenize_raw_events(raw_column="raw")
+            )
+            .select(
+                parsers.parse_parameters_from_tokens().alias("parameters"),
+                parsers.parse_user_id_from_tokens().alias("user_id"),
+                parsers.parse_timestamp_from_tokens().alias("timestamp"),
+            )
+            .select(
+                "user_id",
+                "timestamp",
+                parsers.parse_param_value(key="a").alias("article_id"),
+                parsers.parse_param_value(key="c").alias("wiki_id"),
+            )
         )
 
     def filter_events(self, input_data: DataFrame) -> DataFrame:
@@ -52,36 +61,47 @@ class ArticleWikisVisitTransformer(DataTransformer):
         self._log_info("creating users profiles")
         data = self.find_edge_events(input_data)
         data = self.reduce_users_events(data)
+        is_same_wiki = F.col("first_event").getItem(1) == F.col("last_event").getItem(1)
         is_same_article = (
-            (F.col("first_wiki") == F.col("last_wiki")) &
-            (F.col("first_article") == F.col("last_article"))
-        )
-        is_same_wiki = F.col("first_wiki") == F.col("last_wiki")
-        return (
-            data
-            .withColumn("is_same_wiki", F.when(is_same_wiki, True).otherwise(False))
-            .withColumn("is_same_article", F.when(is_same_article, True).otherwise(False))
-        )
+            F.col("first_event").getItem(0) == F.col("last_event").getItem(0)
+        ) & is_same_wiki
+        return data.withColumn(
+            "is_same_wiki", F.when(is_same_wiki, True).otherwise(False)
+        ).withColumn("is_same_article", F.when(is_same_article, True).otherwise(False))
 
-    def find_edge_events(self, input_data: DataFrame) -> DataFrame:
+    def find_edge_events(
+        self, input_data: DataFrame, user_column: str = "user_id"
+    ) -> DataFrame:
+        user_window = (
+            Window()
+            .partitionBy(user_column)
+            .rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+        )
         return (
             input_data.orderBy("timestamp", ascending=True)
-            .withColumn("first_article", func.get_first_user_event_value("article_id"))
-            .withColumn("last_article", func.get_last_user_event_value("article_id"))
-            .withColumn("first_wiki", func.get_first_user_event_value("wiki_id"))
-            .withColumn("last_wiki", func.get_last_user_event_value("wiki_id"))
+            .select(
+                "*",
+                F.array(F.col("article_id"), F.col("wiki_id")).alias(
+                    "article_wiki_ids"
+                ),
+            )
+            .select(
+                "user_id",
+                F.first("article_wiki_ids").over(user_window).alias("first_event"),
+                F.last("article_wiki_ids").over(user_window).alias("last_event"),
+            )
         )
 
     def reduce_users_events(self, input_data: DataFrame) -> DataFrame:
         return input_data.drop_duplicates(
-            ["user_id", "first_article", "last_article", "first_wiki", "last_wiki"]
+            ["user_id", "first_event", "last_event"]
         )
 
     def finalize(self, input_data: DataFrame) -> DataFrame:
         self._log_info("finalize data")
-        return input_data.select(
-            "user_id", "is_same_article", "is_same_wiki"
-        ).orderBy("user_id", ascending=True)
+        return input_data.select("user_id", "is_same_article", "is_same_wiki").orderBy(
+            "user_id", ascending=True
+        )
 
     def _log_info(self, msg: str):
         if ModuleLogger.logger:
